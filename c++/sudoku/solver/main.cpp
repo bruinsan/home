92c7e9470b6fc9e1e327c7332c24585b57bc8bf7
bruinsan <bruinsan@gmail.com>
2017-09-12 22:44:26 +0200

    Sudoku solver zith input datas and output solutions

---
+++ c++/sudoku/solver/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+// #include <grid.h>
+#include <test.h>
+
+using namespace std;
+
+int main()
+{
+    cout << "This is a sudoku solver" << endl;
+
+    test t = new test();
+
+    return 0;
+}
