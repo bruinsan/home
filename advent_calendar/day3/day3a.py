6d94d849646fe96ad730aac1b00e17e6ac5ee6df
bruinsan <bruinsan@gmail.com>
2017-12-08 13:59:43 +0100

    create memory finished

--- advent_calendar/day3/day3a.py
+++ advent_calendar/day3/day3a.py
@@ -3,38 +3,46 @@ from pprint import pprint
 
 #memory_position = sys.argv[1]
 
-def move_right(mem, x, y, actual_nb, times):
+
+def move_right(mem, x, y, actual_nb, times, final_pos):
 	for i in xrange(times):
 		actual_nb += 1
-		mem[x+i+1][y] = actual_nb
+	#	print x+i+1
+		if actual_nb <= final_pos:
+			mem[y][x + i + 1] = actual_nb
 
-	return x+times, y, actual_nb
+	return x + times, y, actual_nb
 
-def move_up(mem, x, y, actual_nb, times):
+
+def move_up(mem, x, y, actual_nb, times, final_pos):
 	for i in xrange(times):
 		actual_nb += 1
-		mem[x][y+i+1] = actual_nb
-		
-	return x, y+times, actual_nb
+		if actual_nb <= final_pos:
+			mem[y + i + 1][x] = actual_nb
 
-def move_left(mem, x, y, actual_nb, times):
+	return x, y + times, actual_nb
+
+
+def move_left(mem, x, y, actual_nb, times, final_pos):
 	for i in xrange(times):
 		actual_nb += 1
-		mem[x-i-1][y] = actual_nb
+		if actual_nb <= final_pos:
+			mem[y][x - i - 1] = actual_nb
 
-	return x-times, y, actual_nb
+	return x - times, y, actual_nb
 
-def move_down(mem, x, y, actual_nb, times):
+
+def move_down(mem, x, y, actual_nb, times, final_pos):
 	for i in xrange(times):
 		actual_nb += 1
-		mem[x][y-i-1] = actual_nb
+		if actual_nb <= final_pos:
+			mem[y - i - 1][x] = actual_nb
 
-	return x, y-times, actual_nb
-
+	return x, y - times, actual_nb
 
 
 def create_spiral_memory(final_pos):
-	memory = [[0 for x in xrange(11)] for y in xrange(11)]	# creating 11x11 matrix
+	memory = [[0 for x in xrange(10)] for y in xrange(10)]  # creating 11x11 matrix
 
 	# RU
 	x_init = 5
@@ -42,26 +50,27 @@ def create_spiral_memory(final_pos):
 	start_num = 1
 	memory[x_init][y_init] = start_num
 	add = 1
-	
-	while True:	# change the condition for stopping when final_pos achieved
-		# RU --> LD --> RUi
-		print "x_init = {} \t y_init = {} \t start_num = {}".format(x_init, y_init, start_num)
-		pprint(memory)
-		print ""
 
-		x_init, y_init, start_num  = move_right(memory, x_init, y_init, start_num, add)
-#		x_init, y_init, start_num  = move_up(memory, x_init, y_init, start_num, add)
-#		x_init, y_init, start_num  = move_left(memory, x_init, y_init, start_num, add+1)
-#		x_init, y_init, start_num  = move_down(memory, x_init, y_init, start_num, add+1)
-		
+	while start_num <= final_pos:  # change the condition for stopping when final_pos achieved
+		# RU --> LD --> RU
+		#		print "x_init = {} \t y_init = {} \t start_num = {}".format(x_init, y_init, start_num)
+		print "updating..."
+		x_init, y_init, start_num = move_right(memory, x_init, y_init, start_num, add, final_pos)
+		x_init, y_init, start_num = move_down(memory, x_init, y_init, start_num, add, final_pos)
+		x_init, y_init, start_num = move_left(memory, x_init, y_init, start_num, add + 1, final_pos)
+		x_init, y_init, start_num = move_up(memory, x_init, y_init, start_num, add + 1, final_pos)
+
 		add += 2
 
-	print memory
+		#print start_num
+		#print final_pos
+	#	print ""
+#	print memory
+	pprint(memory)
+
 
 create_spiral_memory(1)
-#create_spiral_memory(2)
-#create_spiral_memory(3)
+create_spiral_memory(2)
+create_spiral_memory(3)
 #create_spiral_memory(4)
-#create_spiral_memory(5)
-
-
+#create_spiral_memory(1024)
