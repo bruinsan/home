fc33dfb6f0b856b7cd8869172293ab8306c648ef
bruinsan <bruinsan@gmail.com>
2017-12-11 14:39:47 +0100

    dist calculations

--- advent_calendar/day3/day3a.py
+++ advent_calendar/day3/day3a.py
@@ -42,7 +42,10 @@ def move_down(mem, x, y, actual_nb, times, final_pos):
 
 
 def create_spiral_memory(final_pos):
-	memory = [[0 for x in xrange(10)] for y in xrange(10)]  # creating 11x11 matrix
+	size = int(sys.argv[2])
+	max = int(sys.argv[3])
+	memory = [[max for x in xrange(size)]
+           for y in xrange(size)]  # creating 11x11 matrix
 
 	# RU
 	x_init = 5
@@ -53,40 +56,61 @@ def create_spiral_memory(final_pos):
 
 	while start_num <= final_pos:  # change the condition for stopping when final_pos achieved
 		# RU --> LD --> RU
-		#		print "x_init = {} \t y_init = {} \t start_num = {}".format(x_init, y_init, start_num)
-		print "updating..."
-		x_init, y_init, start_num = move_right(memory, x_init, y_init, start_num, add, final_pos)
-		x_init, y_init, start_num = move_down(memory, x_init, y_init, start_num, add, final_pos)
-		x_init, y_init, start_num = move_left(memory, x_init, y_init, start_num, add + 1, final_pos)
-		x_init, y_init, start_num = move_up(memory, x_init, y_init, start_num, add + 1, final_pos)
+		x_init, y_init, start_num = move_right(
+			memory, x_init, y_init, start_num, add, final_pos)
+		x_init, y_init, start_num = move_down(
+			memory, x_init, y_init, start_num, add, final_pos)
+		x_init, y_init, start_num = move_left(
+			memory, x_init, y_init, start_num, add + 1, final_pos)
+		x_init, y_init, start_num = move_up(
+			memory, x_init, y_init, start_num, add + 1, final_pos)
 
 		add += 2
 
-		#print start_num
-		#print final_pos
-	#	print ""
-#	print memory
 	return memory
 
+"""
+def find_number(matrix, nb):
+	for i, row in enumerate(matrix):
+		for j, col in enumerate(row):
+			if col == nb:
+				return i, j
+"""
 
 def find_min(memory, number):
+	#y, x = find_number(memory, number)
+	for i, row in enumerate(memory):
+			for j, col in enumerate(row):
+				if col == number:
+					y = i
+					x = j
 
-	return 0
-	pass
+	print memory[x + 1][y], memory[x - 1][y], memory[x][y + 1], memory[x][y - 1]
+	min_nb = min(memory[x + 1][y], memory[x - 1][y],
+	             memory[x][y + 1], memory[x][y - 1])
+
+	#print "min neighbor = {}".format(min_nb)
+	return min_nb
+
 
 def calculate_manhattan_distance(nb, memory):
 	# find minimum neighbor
 	dist = 0
 	neighbor = -1
+	print memory
+	print ""
 	while neighbor != 1:
+		# print memory, nb
 		neighbor = find_min(memory, nb)
 		dist += 1
 		nb = neighbor
+		# print nb
 
-pprint(create_spiral_memory(1))
-memory = create_spiral_memory(2)
-pprint(create_spiral_memory(3))
-#create_spiral_memory(4)
-#create_spiral_memory(1024)
+	return dist
 
-
+final_num = int(sys.argv[1])
+memory = create_spiral_memory(final_num)
+#print memory
+#pprint(memory)
+print calculate_manhattan_distance(final_num, memory)
+#find_min(memory, 9)
