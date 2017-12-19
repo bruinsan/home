4c394b9c9413a2e53f21ac9539678dd7a7e128bb
bruinsan <bruinsan@gmail.com>
2017-12-19 16:46:24 +0100

    update

--- advent_calendar/day3/day3b.py
+++ advent_calendar/day3/day3b.py
@@ -3,46 +3,59 @@ from pprint import pprint
 from math import sqrt, ceil
 
 def move_right(mem, x, y, times, final_pos):
-	for i in xrange(times):
-		positions = [mem[y+1][x-1], mem[y+1][x], mem[y+1][x+1], mem[y][x-1], mem[y][x], mem[y][x+1], mem[y-1][x-1], mem[y-1][x], mem[y-1][x+1]]
-		print positions
-		actual_nb = sum(positions)
-	#	print x+i+1
-		if actual_nb <= final_pos:
-			mem[y][x + i + 1] = actual_nb
+	print "[RIGHT_START] x = {} \t y = {}".format(x, y)
 
-	return x + times, y, actual_nb
+	for i in xrange(times):
+	# 	positions = [mem[y+1][x-1], mem[y+1][x], mem[y+1][x+1], mem[y][x-1], mem[y][x], mem[y][x+1], mem[y-1][x-1], mem[y-1][x], mem[y-1][x+1]]
+	# 	print positions
+		print "[RIGHT] x = {} \t y = {}".format(x+i+1, y)
+	# 	actual_nb = sum(positions)
+	# 	if actual_nb <= final_pos:
+	# 		mem[y][x] = actual_nb
+	# 	# x += 1
+
+	return x+times, y
+
+def move_left(mem, x, y, times, final_pos):
+	print "[LEFT_START] x = {} \t y = {}".format(x, y)
+	for i in xrange(times):
+		print "[LEFT] x = {} \t y = {}".format(x-i-1, y)
+	# 	positions = [mem[y+1][x-1], mem[y+1][x], mem[y+1][x+1], mem[y][x-1], mem[y][x], mem[y][x+1], mem[y-1][x-1], mem[y-1][x], mem[y-1][x+1]]
+	# 	print positions, x, y
+	# 	actual_nb = sum(positions)
+	# 	if actual_nb <= final_pos:
+	# 		mem[y][x - i - 1] = actual_nb
+	# 	x=x-i
+	# # mem[y][x] = "X"
+	return x - times, y
 
 
 def move_up(mem, x, y, times, final_pos):
+	print "[UP_START] x = {} \t y = {}".format(x, y)
+
 	for i in xrange(times):
-		positions = [mem[y+1][x-1], mem[y+1][x], mem[y+1][x+1], mem[y][x-1], mem[y][x], mem[y][x+1], mem[y-1][x-1], mem[y-1][x], mem[y-1][x+1]]
-		actual_nb = sum(positions)
-		if actual_nb <= final_pos:
-			mem[y + i + 1][x] = actual_nb
+		print "[UP] x = {} \t y = {}".format(x, y-i-1)
+	# 	positions = [mem[y+1][x-1], mem[y+1][x], mem[y+1][x+1], mem[y][x-1], mem[y][x], mem[y][x+1], mem[y-1][x-1], mem[y-1][x], mem[y-1][x+1]]
+	# 	print positions
+	# 	print "x = {} \t y = {}".format(x, y)
 
-	return x, y + times, actual_nb
+	# 	actual_nb = sum(positions)
+	# 	if actual_nb <= final_pos:
+	# 		mem[y][x] = actual_nb
 
-
-def move_left(mem, x, y, times, final_pos):
-	for i in xrange(times):
-		positions = [mem[y+1][x-1], mem[y+1][x], mem[y+1][x+1], mem[y][x-1], mem[y][x], mem[y][x+1], mem[y-1][x-1], mem[y-1][x], mem[y-1][x+1]]
-		actual_nb = sum(positions)
-		if actual_nb <= final_pos:
-			mem[y][x - i - 1] = actual_nb
-
-	return x - times, y, actual_nb
-
+	return x, y - times
 
 def move_down(mem, x, y, times, final_pos):
+	print "[DOWN_START] x = {} \t y = {}".format(x, y)
+
 	for i in xrange(times):
-		positions = [mem[y+1][x-1], mem[y+1][x], mem[y+1][x+1], mem[y][x-1], mem[y][x], mem[y][x+1], mem[y-1][x-1], mem[y-1][x], mem[y-1][x+1]]
-		actual_nb = sum(positions)
-		if actual_nb <= final_pos:
-			mem[y - i - 1][x] = actual_nb
+		print "[DOWN] x = {} \t y = {}".format(x, y+i+1)
+	# 	positions = [mem[y+1][x-1], mem[y+1][x], mem[y+1][x+1], mem[y][x-1], mem[y][x], mem[y][x+1], mem[y-1][x-1], mem[y-1][x], mem[y-1][x+1]]
+	# 	actual_nb = sum(positions)
+	# 	if actual_nb <= final_pos:
+	# 		mem[y + i + 1][x] = actual_nb
 
-	return x, y - times, actual_nb
-
+	return x, y + times
 
 def create_spiral_memory(final_pos):
 	size = int(sys.argv[2])
@@ -61,21 +74,14 @@ def create_spiral_memory(final_pos):
 	add = 1
 
 	while start_num <= final_pos:  # change the condition for stopping when final_pos achieved
-		pprint(memory)
-		# RU --> LD --> RU
-		print x_init, y_init
-		x_init, y_init, start_num = move_right(memory, x_init, y_init, add, final_pos)
-		print x_init, y_init
-		x_init, y_init, start_num = move_down(memory, x_init, y_init, add, final_pos)
-		print x_init, y_init
-		x_init, y_init, start_num = move_left(memory, x_init, y_init, add+1, final_pos)
-		print x_init, y_init
-		x_init, y_init, start_num = move_up(memory, x_init, y_init, add+1, final_pos)
-		print x_init, y_init
+		x_init, y_init = move_right(memory, x_init, y_init, add, final_pos)
+		x_init, y_init = move_up(memory, x_init, y_init, add, final_pos)
+		x_init, y_init = move_left(memory, x_init, y_init, add+1, final_pos)
+		x_init, y_init = move_down(memory, x_init, y_init, add+1, final_pos)
 
 		add += 2
-
-#		move_right(memory, x_init, y_init, add, final_pos)
+		pprint(memory)
+		start_num += 1
 	return memory
 
 
