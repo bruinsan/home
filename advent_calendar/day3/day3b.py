7995411a2562f165d2d0f62debb16593a580f53d
bruinsan <bruinsan@gmail.com>
2017-12-20 13:53:49 +0100

    update

--- advent_calendar/day3/day3b.py
+++ advent_calendar/day3/day3b.py
@@ -2,60 +2,78 @@ import sys
 from pprint import pprint
 from math import sqrt, ceil
 
+
 def move_right(mem, x, y, times, final_pos):
 	print "[RIGHT_START] x = {} \t y = {}".format(x, y)
 
 	for i in xrange(times):
-	# 	positions = [mem[y+1][x-1], mem[y+1][x], mem[y+1][x+1], mem[y][x-1], mem[y][x], mem[y][x+1], mem[y-1][x-1], mem[y-1][x], mem[y-1][x+1]]
-	# 	print positions
-		print "[RIGHT] x = {} \t y = {}".format(x+i+1, y)
-	# 	actual_nb = sum(positions)
-	# 	if actual_nb <= final_pos:
-	# 		mem[y][x] = actual_nb
+		print "[RIGHT] x = {} \t y = {}".format(x + i + 1, y)
+		r = x + i + 1
+		positions = [mem[y + 1][r - 1], mem[y + 1][r], mem[y + 1][r + 1], mem[y][r - 1],
+                    mem[y][r], mem[y][r + 1], mem[y - 1][r - 1], mem[y - 1][r], mem[y - 1][r + 1]]
+		print positions
+		actual_nb = sum(positions)
+		if actual_nb <= final_pos:
+			print actual_nb
+			mem[y][r] = actual_nb
 	# 	# x += 1
 
-	return x+times, y
+	return x + times, y, actual_nb
+
 
 def move_left(mem, x, y, times, final_pos):
-	print "[LEFT_START] x = {} \t y = {}".format(x, y)
+	# print "[LEFT_START] x = {} \t y = {}".format(x, y)
 	for i in xrange(times):
-		print "[LEFT] x = {} \t y = {}".format(x-i-1, y)
-	# 	positions = [mem[y+1][x-1], mem[y+1][x], mem[y+1][x+1], mem[y][x-1], mem[y][x], mem[y][x+1], mem[y-1][x-1], mem[y-1][x], mem[y-1][x+1]]
+		# print "[LEFT] x = {} \t y = {}".format(x-i-1, y)
+		r = x - i - 1
+		positions = [mem[y + 1][r - 1], mem[y + 1][r], mem[y + 1][r + 1], mem[y][r - 1],
+                    mem[y][r], mem[y][r + 1], mem[y - 1][r - 1], mem[y - 1][r], mem[y - 1][r + 1]]
 	# 	print positions, x, y
-	# 	actual_nb = sum(positions)
-	# 	if actual_nb <= final_pos:
+		actual_nb = sum(positions)
+		if actual_nb <= final_pos:
+			# print actual_nb
+			mem[y][r] = actual_nb
 	# 		mem[y][x - i - 1] = actual_nb
 	# 	x=x-i
-	# # mem[y][x] = "X"
-	return x - times, y
+
+	return x - times, y, actual_nb
 
 
 def move_up(mem, x, y, times, final_pos):
-	print "[UP_START] x = {} \t y = {}".format(x, y)
+	# print "[UP_START] x = {} \t y = {}".format(x, y)
 
 	for i in xrange(times):
-		print "[UP] x = {} \t y = {}".format(x, y-i-1)
-	# 	positions = [mem[y+1][x-1], mem[y+1][x], mem[y+1][x+1], mem[y][x-1], mem[y][x], mem[y][x+1], mem[y-1][x-1], mem[y-1][x], mem[y-1][x+1]]
+		# print "[UP] x = {} \t y = {}".format(x, y-i-1)
+		s = y - i - 1
+		positions = [mem[s + 1][x - 1], mem[s + 1][x], mem[s + 1][x + 1], mem[s][x - 1],
+                    mem[s][x], mem[s][x + 1], mem[s - 1][x - 1], mem[s - 1][x], mem[s - 1][x + 1]]
 	# 	print positions
 	# 	print "x = {} \t y = {}".format(x, y)
 
-	# 	actual_nb = sum(positions)
-	# 	if actual_nb <= final_pos:
-	# 		mem[y][x] = actual_nb
+		actual_nb = sum(positions)
+		if actual_nb <= final_pos:
+			# print actual_nb
+			mem[s][x] = actual_nb
 
-	return x, y - times
+	return x, y - times, actual_nb
+
 
 def move_down(mem, x, y, times, final_pos):
-	print "[DOWN_START] x = {} \t y = {}".format(x, y)
+	# print "[DOWN_START] x = {} \t y = {}".format(x, y)
 
 	for i in xrange(times):
-		print "[DOWN] x = {} \t y = {}".format(x, y+i+1)
-	# 	positions = [mem[y+1][x-1], mem[y+1][x], mem[y+1][x+1], mem[y][x-1], mem[y][x], mem[y][x+1], mem[y-1][x-1], mem[y-1][x], mem[y-1][x+1]]
-	# 	actual_nb = sum(positions)
-	# 	if actual_nb <= final_pos:
+		# print "[DOWN] x = {} \t y = {}".format(x, y+i+1)
+		s = y + i + 1
+		positions = [mem[s + 1][x - 1], mem[s + 1][x], mem[s + 1][x + 1], mem[s][x - 1],
+                    mem[s][x], mem[s][x + 1], mem[s - 1][x - 1], mem[s - 1][x], mem[s - 1][x + 1]]
+		actual_nb = sum(positions)
+		if actual_nb <= final_pos:
+			# print actual_nb
+			mem[s][x] = actual_nb
 	# 		mem[y + i + 1][x] = actual_nb
 
-	return x, y + times
+	return x, y + times, actual_nb
+
 
 def create_spiral_memory(final_pos):
 	size = int(sys.argv[2])
@@ -69,23 +87,29 @@ def create_spiral_memory(final_pos):
 	# RU
 	x_init = len(memory) / 2 - 1
 	y_init = len(memory) / 2 - 1
-	start_num = 1
-	memory[x_init][y_init] = start_num
+	actual_nb = 1
+	memory[x_init][y_init] = actual_nb
 	add = 1
 
-	while start_num <= final_pos:  # change the condition for stopping when final_pos achieved
-		x_init, y_init = move_right(memory, x_init, y_init, add, final_pos)
-		x_init, y_init = move_up(memory, x_init, y_init, add, final_pos)
-		x_init, y_init = move_left(memory, x_init, y_init, add+1, final_pos)
-		x_init, y_init = move_down(memory, x_init, y_init, add+1, final_pos)
+	while actual_nb <= final_pos:  # change the condition for stopping when final_pos achieved
+		x_init, y_init, actual_nb = move_right(
+			memory, x_init, y_init, add, final_pos)
+		print "x = {} \t y = {} \t actual_nb = {}".format(x_init, y_init, actual_nb)
+		x_init, y_init, actual_nb = move_up(memory, x_init, y_init, add, final_pos)
+		print "x = {} \t y = {} \t actual_nb = {}".format(x_init, y_init, actual_nb)
+		x_init, y_init, actual_nb = move_left(
+			memory, x_init, y_init, add + 1, final_pos)
+		print "x = {} \t y = {} \t actual_nb = {}".format(x_init, y_init, actual_nb)
+		x_init, y_init, actual_nb = move_down(
+			memory, x_init, y_init, add + 1, final_pos)
+		print "x = {} \t y = {} \t actual_nb = {}".format(x_init, y_init, actual_nb)
 
 		add += 2
-		pprint(memory)
-		start_num += 1
+		# pprint(memory)
 	return memory
 
 
 final_num = int(sys.argv[1])
-memory = create_spiral_memory(final_num)
-pprint(memory)
+m = create_spiral_memory(final_num)
+pprint(m)
 #print calculate_manhattan_distance(final_num, memory)
