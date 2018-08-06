3d1da5bec2a8116e78c5d8c362a0e6be6383fea9
bruinsan <bruinsan@gmail.com>
2018-08-06 20:53:17 +0200

    keep trying faster algo to multiples

--- hacker_rank/project_euler/1.cpp
+++ hacker_rank/project_euler/1.cpp
@@ -22,73 +22,73 @@ uint64_t sum_multiple_3_5(int nb)
     return sum;
 }
 
-uint64_t quick_master_3_5(int nb)
+uint64_t vector_sum(std::vector<uint64_t> vec)
 {
-    uint64_t sum = 0;
-    for(int i=3 ; i<nb ; i++)
-    {
-        if(i%3 == 0)
-        {
-            sum += i;
-        }
-        else{
-            if(i%5 ==0)
-            {
-                sum += i;
-            }
-        }
-    }
-    return sum;
-}
-
-int vector_sum(std::vector<int> vec)
-{
-    int sum_elems = 0;
+    uint64_t sum_elems = 0;
     for(auto& n : vec) sum_elems += n;
     return sum_elems;
 }
 
-int quick_multiple_3_5(int limit_seq)
+uint64_t quick_master_3_5(int nb)
 {
-    int multiple = 1;
-    std::vector<int> vec_multiples;
-    vec_multiples.push_back(0);
-    while(vec_multiples.back() < limit_seq)
+    uint64_t mul_3 = (nb-1)/3;
+    uint64_t mul_5 = (nb-1)/5;
+    std::vector<uint64_t> vec_mul;
+    for (int i=1 ; i<= mul_3 ; i++)
     {
-//        std::cout << "multiple value --> " << multiple << std::endl;
-        int aux = multiple*3;
-        if(std::find(vec_multiples.begin(), vec_multiples.end(), aux) == vec_multiples.end())
-        {// multiples vector does not have the element, so we push back it
- //           std::cout << "aux value --> " << aux << std::endl;
-            vec_multiples.push_back(aux);
-        }
+        vec_mul.push_back(i*3);
+    }
+
+    for (int i=1 ; i<= mul_5 ; i++)
+    {
+        vec_mul.push_back(i*5);
+    }
     
-        aux = multiple*5;
-        if(std::find(vec_multiples.begin(), vec_multiples.end(), aux) == vec_multiples.end())
-        {// multiples vector does not have the element, so we push back it
- //           std::cout << "aux value --> " << aux << std::endl;
-            vec_multiples.push_back(aux);
-        }
-
-        multiple++;
-    }
-   
-    vec_multiples.pop_back(); // remove last element, it is greater than the limit
-    int aux = multiple*3;
-    if(std::find(vec_multiples.begin(), vec_multiples.end(), aux) == vec_multiples.end())
-    {// multiples vector does not have the element, so we push back it
- //       std::cout << "aux value --> " << aux << std::endl;
-        vec_multiples.push_back(aux);
-    }
-//    std::cout << "printing vector" << std::endl;
-//    for(std::vector<int>::iterator it = vec_multiples.begin() ; it != vec_multiples.end() ; it++)
-//    {
-//        std::cout << *it << std::endl;
-//    }
-//    std::cout << "vector's sum --> " << vector_sum(vec_multiples) << std::endl;
-    return vector_sum(vec_multiples);
-
+    std::cout << vector_sum(vec_mul) << std::endl;
 }
+
+
+//int quick_multiple_3_5(int limit_seq)
+//{
+//    int multiple = 1;
+//    std::vector<int> vec_multiples;
+//    vec_multiples.push_back(0);
+//    while(vec_multiples.back() < limit_seq)
+//    {
+////        std::cout << "multiple value --> " << multiple << std::endl;
+//        int aux = multiple*3;
+//        if(std::find(vec_multiples.begin(), vec_multiples.end(), aux) == vec_multiples.end())
+//        {// multiples vector does not have the element, so we push back it
+// //           std::cout << "aux value --> " << aux << std::endl;
+//            vec_multiples.push_back(aux);
+//        }
+//    
+//        aux = multiple*5;
+//        if(std::find(vec_multiples.begin(), vec_multiples.end(), aux) == vec_multiples.end())
+//        {// multiples vector does not have the element, so we push back it
+// //           std::cout << "aux value --> " << aux << std::endl;
+//            vec_multiples.push_back(aux);
+//        }
+//
+//        multiple++;
+//    }
+//   
+//    vec_multiples.pop_back(); // remove last element, it is greater than the limit
+//    int aux = multiple*3;
+//    if(std::find(vec_multiples.begin(), vec_multiples.end(), aux) == vec_multiples.end())
+//    {// multiples vector does not have the element, so we push back it
+// //       std::cout << "aux value --> " << aux << std::endl;
+//        vec_multiples.push_back(aux);
+//    }
+////    std::cout << "printing vector" << std::endl;
+////    for(std::vector<int>::iterator it = vec_multiples.begin() ; it != vec_multiples.end() ; it++)
+////    {
+////        std::cout << *it << std::endl;
+////    }
+////    std::cout << "vector's sum --> " << vector_sum(vec_multiples) << std::endl;
+////    return vector_sum(vec_multiples);
+//
+//}
 
 int main()
 {
@@ -109,7 +109,8 @@ int main()
     { 
       //  std::cout << sum_multiple_3_5(*it) << std::endl;
       //  std::cout << "quick calculus" << std::endl;
-        std::cout << quick_multiple_3_5(*it) << std::endl;
+//        std::cout << quick_multiple_3_5(*it) << std::endl;
+        quick_master_3_5(*it);
     }
 
     return 0;
