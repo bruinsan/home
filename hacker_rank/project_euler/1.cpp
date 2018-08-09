6ca7e63692db765051bf065b33f2d5a02aea2ed7
bruinsan <bruinsan@gmail.com>
2018-08-09 13:55:23 +0200

    finished: multiples of 3 or 5

--- hacker_rank/project_euler/1.cpp
+++ hacker_rank/project_euler/1.cpp
@@ -31,31 +31,20 @@ uint64_t vector_sum(std::vector<uint64_t> vec)
 
 uint64_t quick_master_3_5(int nb)
 {
-    float mul_3 = (nb-1)/3;
-    float mul_5 = (nb-1)/5;
-    float mul_15 = (nb-1)/15;
+    uint64_t mul_3 = (nb-1)/3;
+    uint64_t mul_5 = (nb-1)/5;
+    uint64_t mul_15 = (nb-1)/15;
 
-    float mmm3 = mul_3 * 3;
-    float mmm5 = mul_5 * 5;
-    float mmm15 = mul_15 * 15;
+    uint64_t mmm3 = mul_3 * 3;
+    uint64_t mmm5 = mul_5 * 5;
+    uint64_t mmm15 = mul_15 * 15;
 
-    float sum = 0;
+    uint64_t sum = 0;
     
-//    uint64_t mul_3 = (nb-1)/3;
-//    uint64_t mul_5 = (nb-1)/5;
-//    uint64_t mul_15 = (nb-1)/15;
-//
-//    uint64_t mmm3 = mul_3 * 3;
-//    uint64_t mmm5 = mul_5 * 5;
-//    uint64_t mmm15 = mul_15 * 15;
-//
-//    uint64_t sum = 0;
-
-    sum += ((3 + mmm3) / 2) * mul_3;
-    //std::cout << sum << std::endl;
-    sum += ((5 + mmm5) / 2) * mul_5;
-    sum -= ((15 + mmm15) / 2) * mul_15;
-
+    sum += ((3 + mmm3)  * mul_3) / 2;
+    sum += ((5 + mmm5) * mul_5) / 2;
+    sum -= ((15 + mmm15) * mul_15) / 2;
+    
     return sum;
 
 }
@@ -78,10 +67,8 @@ int main()
 
     for(it = vec_nb.begin() ; it != vec_nb.end() ; it++)
     { 
-      //    std::cout << sum_multiple_3_5(*it) << std::endl;
-      //  std::cout << "quick calculus" << std::endl;
-//        std::cout << quick_multiple_3_5(*it) << std::endl;
           std::cout << quick_master_3_5(*it) << std::endl;
+          std::cout << sum_multiple_3_5(*it) << std::endl;
     }
 
     return 0;
